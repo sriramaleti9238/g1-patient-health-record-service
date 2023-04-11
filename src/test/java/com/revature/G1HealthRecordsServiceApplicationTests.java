8f1a58c3849f20a3c7513fc08e41e64bc24eb8b0
sriramaleti9238 <sri13042000@gmail.com>
2023-04-12 05:18:54 +0530

    health-records

---
+++ src/test/java/com/revature/G1HealthRecordsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1HealthRecordsServiceApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+}
